80b67a7076c4d1e71a3f55eae724f4281d005556
rafanomezantsoa <mahery.rafanomezantsoa@epitech.eu>
2020-03-27 12:43:37 +0100

    Updates

---
+++ precache-manifest.a48aabbc0141df16e6601f1cc626c013.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cb6a622a4dd4a37c20764695d225e4",
+    "url": "/dotcom/index.html"
+  },
+  {
+    "revision": "84fb2b3061b0faa54f80",
+    "url": "/dotcom/static/css/main.5972d4e0.chunk.css"
+  },
+  {
+    "revision": "a074e6b069b9d8056ade",
+    "url": "/dotcom/static/js/2.76a3cb19.chunk.js"
+  },
+  {
+    "revision": "989ac8b04042782519a29910086adb47",
+    "url": "/dotcom/static/js/2.76a3cb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fb2b3061b0faa54f80",
+    "url": "/dotcom/static/js/main.7f38bcd8.chunk.js"
+  },
+  {
+    "revision": "2ed3f06c404a5de38ef9",
+    "url": "/dotcom/static/js/runtime-main.9acd12d0.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/dotcom/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/dotcom/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/dotcom/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/dotcom/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "/dotcom/static/media/up-arrow-circle.24e8d8f2.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/dotcom/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
